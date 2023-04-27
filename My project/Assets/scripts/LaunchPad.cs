2a2c02732b464afcb24b666d57d0424b49ad815a
Adrian Montes <adrian.montes@digipen.edu>
2023-04-27 16:07:20 +0200

    More launchpad properties
    Cooldown added to launchpads instead of making them dissappear

--- My project/Assets/scripts/LaunchPad.cs
+++ My project/Assets/scripts/LaunchPad.cs
@@ -9,4 +9,45 @@ public class LaunchPad : MonoBehaviour
     public float mAccuracyValue = 0;
     public bool mExtraJump = true;
     public bool mLastLP = false;
+    public Vector2 mRandomOffset = new Vector2(0, 0);
+    public Vector2 mVelocity = new Vector2(0, 0);
+    public Vector2 mMovementRange = new Vector2(0, 0);
+    public Vector3 mStartingPos = new Vector3(0, 0, 0);
+    public float mJumpBarFactor = 1f;
+    public bool mTriggered = false;
+    public bool mCancelPlayerVel = false;
+
+    [SerializeField] private Material mCooldownMat;
+    [SerializeField] private Material mNormalMat;
+
+    private float mCd = 0f;
+    private float mMaxCD = 3f;
+    private float mT = 0f;
+    private void Update()
+    {
+        transform.position = mStartingPos + new Vector3(Mathf.Sin(mVelocity.x * mT) * mMovementRange.x, Mathf.Sin(mVelocity.y * mT) * mMovementRange.y, 0);
+
+        mT += Time.deltaTime;
+
+        //Update cooldown
+        if (mCd != 0f)
+        {
+            mCd += Time.deltaTime;
+            if (mCd >= mMaxCD)
+            {
+                mCd = 0f;
+                gameObject.GetComponent<MeshRenderer>().material = mNormalMat;
+            }
+        }
+    }
+
+    public void StartCooldown()
+    {
+        mCd += Time.deltaTime;
+        gameObject.GetComponent<MeshRenderer>().material = mCooldownMat;
+    }
+    public bool IsInCooldown()
+    {
+        return mCd != 0f;
+    }
 }
